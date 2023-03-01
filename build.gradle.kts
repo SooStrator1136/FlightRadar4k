2f902092dea51a9f68c68563d38895713b429849
SooStrator <49093091+SooStrator1136@users.noreply.github.com>
2023-03-01 10:22:11 +0100

    Bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
     `maven-publish`
 }
@@ -12,9 +12,9 @@ version = "1.0"
 repositories.mavenCentral()
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:2.2.2")
+    implementation("io.ktor:ktor-client-core:2.2.4")
     implementation("io.ktor:ktor-client-cio:2.2.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
     testImplementation(kotlin("test"))
 }
